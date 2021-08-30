2f44ca5793d79ab32c464f0f773888fd38b8f8b8
Suyash <suyashsingh1313@gmail.com>
2021-08-30 11:45:25 +0530

    home page development complete

--- js/features.js
+++ js/features.js
@@ -1,3 +1,8 @@
+window.addEventListener("scroll", function(){
+    var nav = document.querySelector('nav');
+    nav.classList.toggle("sticky", window.scrollY > 0);
+})
+
 const burgerSlider = ()=>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('nav')
--- js/interactive-features.js
+++ js/interactive-features.js
@@ -9,7 +9,7 @@ var features_width = initial_width;
 
 window.addEventListener("resize", onresize);
 
-function onresize(){
+function onresize() {
     features_width = features.offsetWidth;
     console.log(features_width);
 }
@@ -20,11 +20,11 @@ document.onkeydown = function (e) {
     // console.log((features_width-movespeed) + 'px');
     switch (e.keyCode) {
         case 68:
-            if (features_width > window.innerWidth){
+            if (features_width > window.innerWidth) {
                 lower.style.transform = "translateX(-" + (moved + moveBy) + 'px' + ")";
                 upper.style.transform = "translateX(-" + (moved + moveBy) + 'px' + ")";
                 moved += moveBy;
-            }   
+            }
             break;
 
         case 65:
@@ -36,21 +36,21 @@ document.onkeydown = function (e) {
             break;
 
         case 87:
-            if (features_width > window.innerWidth){
+            if (features_width > window.innerWidth) {
                 lower.style.transform = "translateX(-" + (moved + movespeed) + 'px' + ")";
                 upper.style.transform = "translateX(-" + (moved + movespeed) + 'px' + ")";
-                features.style.width = "" + (features_width-movespeed) + 'px';
-                features_width-=movespeed;
+                features.style.width = "" + (features_width - movespeed) + 'px';
+                features_width -= movespeed;
                 moved += movespeed;
-            } 
+            }
             break;
 
         case 83:
             lower.style.transform = "translateX(-" + (moved - movespeed) + 'px' + ")";
             upper.style.transform = "translateX(-" + (moved - movespeed) + 'px' + ")";
-            features.style.width = "" + (features_width+movespeed) + 'px';
-            if(features_width<initial_width){
-                features_width+=movespeed;
+            features.style.width = "" + (features_width + movespeed) + 'px';
+            if (features_width < initial_width) {
+                features_width += movespeed;
             }
             if (moved > 0) {
                 moved -= movespeed;
@@ -60,18 +60,23 @@ document.onkeydown = function (e) {
 };
 
 
-gsap.to('.block div', {delay: 7, duration:1, x:'-100%', stagger:0.2})
-gsap.to('.loading', {delay:2.5, duration:0.5, y:'+100px', opacity:0})
-gsap.from('.logo', {opacity:0, duration:1, delay:3});
-gsap.to('.logo', {opacity:0, duration:1, delay:5});
+gsap.to('.block div', { delay: 7, duration: 1, x: '-100%', stagger: 0.2 })
+gsap.to('.loading', { delay: 2.5, duration: 0.5, y: '+100px', opacity: 0 })
+gsap.from('.logo', { opacity: 0, duration: 1, delay: 3 });
+gsap.to('.logo', { opacity: 0, duration: 1, delay: 5 });
 
-gsap.from('.upper .text', {opacity:0, duration: 0.4, y:'-100px', delay:8})
-gsap.from('.car', {x:'-20vw', duration:2, delay:8.5})
+gsap.from('.upper .text', { opacity: 0, duration: 0.4, y: '-100px', delay: 8 })
+gsap.from('.car', { x: '-20vw', duration: 2, delay: 8.5 })
 // gsap.from('.feature', {scrollTrigger:'.feature', opacity:0, duration: 0.4, y:'-100px', delay:1})
 
 var body = document.querySelector('body');
 setTimeout(show_overflow, 8000);
 
-function show_overflow(){
+function show_overflow() {
     body.style.overflowX = "visible";
 }
+
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+});
